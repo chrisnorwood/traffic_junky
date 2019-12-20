968dbff1f20b0c123cd194b696d76f725d4a57bc
Chris Norwood <cnorwood92@gmail.com>
2019-12-20 12:43:47 -0800

    Initialize repo

---
+++ lib/traffic_junky.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require "traffic_junky/version"
+
+module TrafficJunky
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/traffic_junky/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module TrafficJunky
+  VERSION = "0.1.0"
+end
